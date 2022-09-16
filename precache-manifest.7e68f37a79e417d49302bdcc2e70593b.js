48a4934bb3388940cfc67b47be5187b1f4f64495
KaterinkaVysotskaya <katerinka.vysotskaya@gmail.com>
2022-09-16 22:53:10 +0300

    Updates

---
+++ precache-manifest.7e68f37a79e417d49302bdcc2e70593b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adbc90bb53a2a694207188ac36d4cd24",
+    "url": "/homeworks/index.html"
+  },
+  {
+    "revision": "1b00c9bbb2bb9c3bcaa8",
+    "url": "/homeworks/static/css/main.1cf45eab.chunk.css"
+  },
+  {
+    "revision": "b09ec9783f500ebe0a50",
+    "url": "/homeworks/static/js/2.de696e6f.chunk.js"
+  },
+  {
+    "revision": "515b6fe60c498b4dfd7253fe15114795",
+    "url": "/homeworks/static/js/2.de696e6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b00c9bbb2bb9c3bcaa8",
+    "url": "/homeworks/static/js/main.f1560087.chunk.js"
+  },
+  {
+    "revision": "dd098ccd315c55f9daed",
+    "url": "/homeworks/static/js/runtime-main.fb2d67e7.js"
+  },
+  {
+    "revision": "6bc825fcdf585304ab5c750d83cb57a0",
+    "url": "/homeworks/static/media/preloader.6bc825fc.gif"
+  }
+]);
